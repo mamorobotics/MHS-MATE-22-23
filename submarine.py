985f87c0160394ea16f092b1bddde91b13e19ad0
Dabuilda <107159084+TheMagnificentMiles@users.noreply.github.com>
2023-05-08 12:51:15 -0400

    Submarine new controls

--- submarine.py
+++ submarine.py
@@ -31,17 +31,15 @@ def main():
         if conn.output[0] == 12:
             controllerValues = seamoth.ControllerValues.fromString(conn.output[1].decode('utf-8'))
 
-        left = controllerValues.LeftJoystickY
-        right = controllerValues.LeftJoystickY
+        X = -controllerValues.LeftJoystickX * 100
+        Y = controllerValues.LeftJoystickY * 100
+        V = (100 - abs(X)) * (Y / 100) + Y
+        W = (100 - abs(Y)) * (X / 100) + X
+        R = (V + W) / 2
+        L = (V - W) / 2
 
-        if (controllerValues.LeftJoystickX > 0.5):
-            right = -right
-
-        if (controllerValues.LeftJoystickX < -0.5):
-            left = -left
-
-        LF.setSpeed(left)
-        RF.setSpeed(right)
+        LF.setSpeed(L / 100)
+        RF.setSpeed(R / 100)
 
         LU.setSpeed(controllerValues.RightJoystickY)
         RU.setSpeed(controllerValues.RightJoystickY)
